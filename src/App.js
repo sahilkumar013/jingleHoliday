cc0c3f8ad90bfff8d210a5a95786a074906cade0
sahilkumar013 <sahilkashyap7056@gmail.com>
2023-08-22 19:09:57 +0530

    complete

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Route, Router} from 'react-router-dom'
+import { HomePage } from './Components/HomePage';
+
+function App() {
+  return (
+    
+     <HomePage/>
+  
+  );
+}
+
+export default App;
